b4211a44e4465136cdb73b8683ec59421a3bd27e
cb3fias <cb3fias@AP704924.ztb.icb.commerzbank.com>
2018-01-02 17:51:55 +0100

    di1751

--- java/src/com/conetex/contract/lang/function/runtime/Request.java
+++ java/src/com/conetex/contract/lang/function/runtime/Request.java
@@ -1,13 +1,61 @@
 package com.conetex.contract.lang.function.runtime;
 
 import com.conetex.contract.build.Symbols;
+import com.conetex.contract.build.BuildFunctions.Assign;
+import com.conetex.contract.build.BuildFunctions;
+import com.conetex.contract.build.CodeModel;
+import com.conetex.contract.build.CodeNode;
+import com.conetex.contract.build.CodeModel.EggAbstr;
+import com.conetex.contract.build.CodeModel.EggFun;
+import com.conetex.contract.build.CodeModel.EggFunImp;
+import com.conetex.contract.build.exceptionFunction.AbstractInterpreterException;
+import com.conetex.contract.build.exceptionFunction.DublicateOperation;
+import com.conetex.contract.build.exceptionFunction.OperationMeansNotCalled;
+import com.conetex.contract.build.exceptionType.AbstractTypException;
 import com.conetex.contract.lang.function.Accessible;
+import com.conetex.contract.lang.function.access.AccessibleConstant;
+import com.conetex.contract.lang.type.TypeComplex;
 import com.conetex.contract.lang.value.implementation.Structure;
 import com.conetex.contract.run.ContractRuntime;
 import com.conetex.contract.run.exceptionValue.AbstractRuntimeException;
+import com.conetex.contract.run.exceptionValue.Inconvertible;
+import com.conetex.contract.run.exceptionValue.Invalid;
 
 public class Request extends Accessible<String> {
+
+	/*
+	// lambda:
+	public static interface Buildable {
+		public void build();
+	} 
+	public static void register(Buildable f){	}
+	Buildable b = () -> Request.build();
+	CodeModel.register(b);
+	CodeModel.register( () -> Request.build() );
+	CodeModel.register( Request::build );
+	*/
 	
+	private static String getCommandStatic() {
+		return Symbols.COM_REQUEST;
+	}
+	
+	public static void build() throws DublicateOperation, OperationMeansNotCalled{
+		EggFun<String> request = new CodeModel.EggFunImp<String>("stringRequest"){
+			
+			@Override
+			public Accessible<String> functionCreate(CodeNode thisNode, TypeComplex parentType)
+					throws Inconvertible, Invalid, AbstractInterpreterException, AbstractTypException {
+				//return AccessibleConstant.create2(String.class, thisNode);
+				return new Request("eine Frage?");
+			}
+			
+		};
+		request.means(Request.getCommandStatic());
+		//Expression.numberExpession.functionContains(request);
+		BuildFunctions.Assign.whatEverAssigment.functionContains(request);
+		
+	}
+		
 	private final String question;
 		
 	private Request(String theQuestion) {
@@ -32,8 +80,10 @@ public class Request extends Accessible<String> {
 
 	@Override
 	public String getCommand() {
-		return Symbols.COM_REQUEST;
+		return Request.getCommandStatic();
 	}
+
+
 
 	@Override
 	public Accessible<?>[] getChildren() {
