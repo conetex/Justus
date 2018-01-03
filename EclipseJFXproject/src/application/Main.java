062c9c54e34406980504bff57f11a0a33ba3fd07
cb3fias <cb3fias@AP704924.ztb.icb.commerzbank.com>
2018-01-03 18:00:28 +0100

    fx experimente

--- EclipseJFXproject/src/application/Main.java
+++ EclipseJFXproject/src/application/Main.java
@@ -1,16 +1,53 @@
 package application;
 	
 import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.SplitPane;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.StackPane;
 
 
 public class Main extends Application {
 	@Override
 	public void start(Stage primaryStage) {
 		try {
-			BorderPane root = new BorderPane();
+	        primaryStage.setTitle("Hello World!");
+	        
+	        SplitPane splitPane1 = new SplitPane();
+	        //splitPane1.setPrefSize(200, 200);
+	        
+	        HBox leftArea = new HBox();
+	        HBox rightArea = new HBox();
+
+	        
+	        final Button btn = new Button("Left Button");
+	        leftArea.getChildren().add(btn);
+	        
+	        final Button r = new Button("Right Button");
+	        rightArea.getChildren().add(r);
+	        
+	        //splitPane1.getItems().addAll(btn, r);
+	        splitPane1.getItems().add(leftArea);
+	        splitPane1.getItems().add(rightArea);
+	        
+	        
+	        btn.setText("Say 'Hello World'");
+	        btn.setOnAction(new EventHandler<ActionEvent>() {
+	            @Override
+	            public void handle(ActionEvent event) {
+	                System.out.println("Hello World!");
+	            }
+	        });			
+			
+	        StackPane root = new StackPane();
+			//root.getChildren().add(btn);
+			root.getChildren().add(splitPane1);
+			
 			Scene scene = new Scene(root,400,400);
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 			primaryStage.setScene(scene);
