f2dfce0ac76dc6ba31f3e4263694a90585c7b496
Matthias <Matthias@192.168.178.137>
2018-01-09 23:21:10 +0100

    di2321

---
+++ java/src/com/conetex/justus/Pem.java
@@ -0,0 +1,41 @@
+package com.conetex.justus;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.security.KeyFactory;
+import java.security.NoSuchAlgorithmException;
+import java.security.PublicKey;
+import java.security.spec.InvalidKeySpecException;
+import java.security.spec.X509EncodedKeySpec;
+import java.util.Base64;
+
+public class Pem{
+
+	public static void main(String[] args) throws IOException {
+		
+		File f = new File("KeyPair/publicKey");	
+		FileInputStream fis = new FileInputStream(f);
+		
+		byte[] k = fis.readAllBytes();
+		fis.close();
+		
+		byte[] bytes = Base64.getDecoder().decode( k );
+		
+		try{
+			PublicKey publicKey = 
+				    KeyFactory.getInstance("RSA").generatePublic(new X509EncodedKeySpec(bytes));
+		}
+		catch(InvalidKeySpecException e){
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		catch(NoSuchAlgorithmException e){
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+}
